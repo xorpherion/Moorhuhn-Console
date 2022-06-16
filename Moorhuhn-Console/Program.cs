5a78e4b19e31e2fb1475e5b6f3c83326b5c5cf01
till <nogiax@googlemail.com>
2022-06-16 15:13:07 +0200

    initial commit; console responsive rendering

---
+++ Moorhuhn-Console/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+using System.Collections.Concurrent;
+using Moorhuhn_Console;
+
+public class Entry
+{
+    private bool IsRunning { get; set; } = true;
+    private ConcurrentQueue<ConsoleKeyInfo> Inputs { get; } = new();
+
+    private List<Char> inputHistory { get; } = new();
+
+    // private Renderer renderer { get; } = new Renderer(Console.WindowWidth, Console.WindowHeight, 2);
+
+    public static void Main(string[] args)
+    {
+        new Entry().LetsGo(args);
+        
+    }
+
+    public void LetsGo(string[] strings)
+    {
+        StartAndRunInputLoop();
+        RunGameLoop();
+    }
+
+    private void StartAndRunInputLoop()
+    {
+        Task.Factory.StartNew(() =>
+        {
+            while (IsRunning)
+            {
+                var playerInput = Console.ReadKey();
+                Inputs.Enqueue(playerInput);
+            }
+        });
+    }
+
+    private void RunGameLoop()
+    {
+        while (IsRunning)
+        {
+            var inputChar = Inputs.TryDequeue(out ConsoleKeyInfo keyInfo);
+            if (inputChar)
+            {
+                inputHistory.Add(keyInfo.KeyChar);
+            }
+            // Console.WriteLine(String.Join("",inputHistory));
+
+
+            var windowWidth = Console.WindowWidth;
+            var windowHeight = Console.WindowHeight;
+            try
+            {
+                new Renderer(windowWidth, windowHeight, 2).Draw();
+            }
+            catch (Exception e)
+            {
+                //ignore
+            }
+            Thread.Sleep(100);
+        }
+    }
+}
---
+++ Moorhuhn-Console/Renderer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Moorhuhn_Console;
+
+public class Renderer
+{
+    public int ColumnSize { get; }
+    public char[][] Pixels { get; }
+
+    public Renderer(int width, int height, int columnSize)
+    {
+        height--;
+        height--;
+        width--;
+        
+        ColumnSize = columnSize;
+        Pixels = new char[height][];
+        for (int i = 0; i < Pixels.Length; i++)
+        {
+            Pixels[i] = new char[width];
+        }
+
+        for (int y = 0; y < Math.Min(2,height); y++)
+        {
+            for (int x = 0; x < Math.Min(5,width); x++)
+            {
+                Pixels[y][x] = (char) (65+x);
+            }
+        }
+    }
+
+    public void Draw()
+    {
+        Console.SetCursorPosition(0,0);
+        Console.WriteLine(String.Join("",Field()));
+    }
+
+    private String Field()
+    {
+        StringBuilder sb = new();
+        foreach (var row in Pixels)
+        {
+            foreach (var pixel in row)
+                sb.Append(pixel);
+
+            sb.Append('\n');
+        }
+
+        return sb.ToString();
+    }
+
+    private String Clear()
+    {
+        return String.Join("",Pixels.Select(row => "\n").ToList());
+    }
+}
